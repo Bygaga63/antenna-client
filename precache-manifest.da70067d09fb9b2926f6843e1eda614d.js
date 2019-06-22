ea2c9875f1640c3d3fe9fab6c3582b3d6120a084
Dmitry <bygaga63@gmail.com>
2019-06-22 21:34:36 +0300

    Updates

---
+++ precache-manifest.da70067d09fb9b2926f6843e1eda614d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93e8939a473c444564c065cb192760e1",
+    "url": "/antenna12/index.html"
+  },
+  {
+    "revision": "2e9175aa6a4acee7ae1f",
+    "url": "/antenna12/static/css/2.118dfa7b.chunk.css"
+  },
+  {
+    "revision": "4eb944717c16ed562083",
+    "url": "/antenna12/static/css/main.d3c1742d.chunk.css"
+  },
+  {
+    "revision": "2e9175aa6a4acee7ae1f",
+    "url": "/antenna12/static/js/2.ea6d8669.chunk.js"
+  },
+  {
+    "revision": "4eb944717c16ed562083",
+    "url": "/antenna12/static/js/main.3b92a9f1.chunk.js"
+  },
+  {
+    "revision": "4bb44f23635fa82bebc0",
+    "url": "/antenna12/static/js/runtime~main.0c403c3f.js"
+  }
+]);
